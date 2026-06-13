8af9497c017f7bc23868fbd108a4100cfdc4f488
pmohan10 <pramoo@hotmail.com>
2026-06-13 19:18:24 -0400

    comment added

--- src/test/java/paraBank/Login.java
+++ src/test/java/paraBank/Login.java
@@ -18,6 +18,7 @@ public class Login {
         this.passwd = page.locator("[name=\"password\"]");
         this.loginBtn = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Log In"));
     }
+// new comment added, as test
 
     public AcctOvrvw loginPage(){
         page.navigate("https://parabank.parasoft.com/");
